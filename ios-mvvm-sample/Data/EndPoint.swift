dd8b454bde2c30301963c92965e1e4b6a7ca5f3a
clarabae-dev <clarabae.dev@gmail.com>
2020-08-12 18:37:21 +0900

    layer 구분, weather entity 작성, endpoint 작성 중

---
+++ ios-mvvm-sample/Data/EndPoint.swift
@@ -0,0 +1,13 @@
+//
+//  EndPoint.swift
+//  ios-mvvm-sample
+//
+//  Created by seungminbae on 2020/08/12.
+//  Copyright © 2020 seungminbae. All rights reserved.
+//
+
+import Foundation
+
+struct EndPoint {
+    let currentWeather = ""
+}
